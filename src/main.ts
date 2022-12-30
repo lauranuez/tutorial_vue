a5b0703c9b2aaace95619dbd576cc59a9e68a7ca
lauranuez <lauranuezzaragoza@gmail.com>
2022-12-30 21:21:48 +0100

    Step5: Table, emitter, delete, icons

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,16 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
-import BootstrapVue3 from 'bootstrap-vue-3'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import BootstrapVue3  from 'bootstrap-vue-3'
+import  BootstrapVueIcons  from 'bootstrap-vue-3'
 
 
-const app = createApp(App)
-app.use(BootstrapVue3)
-app.mount('#app')
+import mitt from 'mitt';                  // Import mitt
+const emitter = mitt();                   // Initialize mitt
+
+const app = createApp(App);
+app.use(BootstrapVue3);
+app.use(BootstrapVueIcons);
+app.provide('emitter', emitter); 
+app.mount('#app');
